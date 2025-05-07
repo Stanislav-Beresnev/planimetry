1f37509bb9f1a362f0be7c0234616bab67c2c54a
Stanislav Beresnev <berstanislav@gmail.com>
2025-05-07 21:34:23 +0300

    type refactoring

--- src/core/facts/objects/baseObject.ts
+++ src/core/facts/objects/baseObject.ts
@@ -1,14 +1,8 @@
 import { IBaseObject, OBJECT_TYPES } from "@core/facts/types/objectTypes";
 
-/**
- * @description Базовый объект для основных понятий планиметрии;
- */
 export abstract class BaseObject implements IBaseObject {
   abstract readonly type: OBJECT_TYPES;
 
-  /**
-   * @description Получение имени объекта на основании структуры
-   */
   abstract getName(): string;
 
   getTypedName(): string {
--- src/core/facts/types/objectTypes.ts
+++ src/core/facts/types/objectTypes.ts
@@ -1,4 +1,12 @@
+/** @description Базовый объект для основных понятий планиметрии; */
 export interface IBaseObject {
+  /** @description строковый тип объекта; */
+  readonly type: OBJECT_TYPES;
+
+  /** @description возвращает имя объекта на основании структуры; */
+  getName(): string;
+
+  /** @description возвращает имя на основании типа объекта и его структуры; */
   getTypedName(): string;
 }
 
